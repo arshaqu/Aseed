4aa603138c1f841e27fa7346d64af46cb379c8db
arshaqu <you@example.commuhammedarshaque1999@gmail.com>
2024-12-09 12:30:23 +0530

    second commmit

---
+++ src/Pages/components/Banner.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Banner() {
+  return (
+    <div>
+      <div class="relative h-screen">
+  <div class="absolute inset-0 bg-cover bg-center" style="background-image: url('')"></div>
+  <div class="relative z-10 flex flex-col items-center justify-center h-full px-4 sm:px-6 lg:px-8 text-white">
+    <div class="bg-green-500 bg-opacity-75 p-6 rounded-lg text-center">
+      <h1 class="text-4xl font-bold mb-4">Organic Agriculture Farming Products</h1>
+      <p class="mb-6">Dissuade ecstatic and properly saw entirely sir why laughter endeavor. In on my jointure horrible margaret suitable he followed.</p>
+      <a href="#" class="bg-yellow-400 hover:bg-yellow-500 text-gray-800 font-bold py-2 px-4 rounded">Discover More</a>
+    </div>
+  </div>
+</div>
+    </div>
+  )
+}
+
+export default Banner
