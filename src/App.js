1fe9ab36278ad459985afc70174b4c9a0c7da434
arshaqu <you@example.commuhammedarshaque1999@gmail.com>
2024-12-09 11:53:46 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from './Pages/Home';
+import { Button } from 'flowbite-react';
+
+function App() {
+  return (
+    <div className="App">
+      <Home/>
+  
+    </div>
+  );
+}
+
+export default App;
